895cbda4c51a11ec047be020a4f5ea91ba13cedb
SairajKoyande1111 <sairajmca2023_25@ncrdsims.edu.in>
2026-05-10 08:09:35 +0000

    ECO

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,12 @@ module.exports = {
       interpreter: "node",
       env: {
         NODE_ENV: "production",
-        PORT: 3010,
+        PORT: 3013,
+        MONGODB_URI: "mongodb+srv://raneaniket23_db_user:0lEZL6KqIATNmZsj@fishtokricluster.vhw7jp9.mongodb.net/?appName=Fishtokricluster",
+        SESSION_SECRET: "4ht7xAFRChv4cXoq7wxN95DCONA9VrRalzJjMP7QlxcIp4QjKhHl2aC4snWbF5EwL7EKkOSSmyT7NEEEtsQn7Q==",
+        VITE_GOOGLE_MAPS_,
+        AISENSY_API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTcxOTAzYjYyZWI0MGMwMGZkMzNhZiIsIm5hbWUiOiJBVEhBIEZPT0RTIFBSSVZBVEUgTElNSVRFRCIsImFwcE5hbWUiOiJBaVNlbnN5IiwiY2xpZW50SWQiOiI2ODE3MTkwM2I2MmViNDBjMDBmZDMzYWEiLCJhY3RpdmVQbGFuIjoiRlJFRV9GT1JFVkVSIiwiaWF0IjoxNzQ2MzQ0MTk1fQ.NLo_rY9qgxbrE9c3iKRJPcd8mPc5l2NCr-5XdsRUiqM",
+        AISENSY_USERNAME: "ATHA FOODS PRIVATE LIMITED"
       }
     }
   ]
